14ed9b8bd57c623c7f1368c1c08a3e752f0ec981
Anastasiia <na.konsta@gmail.com>
2024-01-14 15:23:51 +0100

    feat(auth): add firt approach to auth

--- public/client.js
+++ public/client.js
@@ -1,52 +1,93 @@
-const wsProto = location.protocol === "https:" ? "wss:" : "ws:";
-const client = new WebSocket(`${wsProto}//${location.host}`);
+let client = null;
 
-const name = faker.internet.userName();
-const messagesContainer = document.getElementById("chat");
+const loginContainer = document.getElementById('login');
+const chatContainer = document.getElementById('chat');
+const newMessageContainer = document.getElementById('new-message-container');
+const newMessage = document.getElementById('new-message');
 
 const addMessage = ({ name, message }) => {
-  const cEl = document.createElement("p");
-  const nEl = document.createElement("span");
-  nEl.className = "name";
+  const cEl = document.createElement('p');
+  const nEl = document.createElement('span');
+  nEl.className = 'name';
   nEl.innerText = name;
   cEl.appendChild(nEl);
 
-  const mEl = document.createElement("span");
-  mEl.className = "message";
+  const mEl = document.createElement('span');
+  mEl.className = 'message';
   mEl.innerText = message;
   cEl.appendChild(mEl);
 
-  messagesContainer.appendChild(cEl);
-  messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  chatContainer.appendChild(cEl);
+  chatContainer.scrollTop = messagesContainer.scrollHeight;
 };
 
-client.addEventListener("message", (message) => {
-  let data;
-  // message.data - это Blob, поэтому его приходится расшифровывать через Blob.text, но вообще это странно
-  try {
-    data = message.data.text().then((txt) => {
-      if (JSON.parse(txt)?.type === "chat_message") {
-        // console.log(`${chalk.bold.green(data.name)}: ${chalk.blue(data.message)}`);
-        addMessage(JSON.parse(txt));
-      }
-    });
-  } catch (err) {
-    return;
-  }
-});
-
-const postMessage = () => {
-  const message = faker.lorem.sentence();
-
+const postMessage = (message) => {
   client.send(
     JSON.stringify({
-      type: "chat_message",
-      name,
+      type: 'chat_message',
       message,
     })
   );
-
-  setTimeout(postMessage, 2000 + (Math.random() - 0.5) * 200);
 };
 
-client.addEventListener("open", postMessage);
+newMessage.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    const value = (newMessage.value || '').trim();
+    if (value) {
+      postMessage(value);
+      newMessage.value = '';
+    }
+  }
+});
+
+loginContainer.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const username = loginContainer.querySelector('.username').value;
+  const password = loginContainer.querySelector('.password').value;
+
+  fetch('/login', {
+    method: 'POST',
+    body: JSON.stringify({ username, password }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return response.text().then((err) => {
+          throw new Error(err);
+        });
+      }
+    })
+    .then(({ token }) => {
+      const wsProto = location.protocol === 'https:' ? 'wss:' : 'ws:';
+      client = new WebSocket(`${wsProto}//${location.host}?token=${token}`);
+
+      loginContainer.style.display = 'none';
+
+      client.addEventListener('open', () => {
+        chatContainer.style.display = 'block';
+        newMessageContainer.style.display = 'block';
+      });
+
+      client.addEventListener('message', (message) => {
+        let data;
+        try {
+          data = message.data;
+          if (JSON.parse(data)?.type === 'chat_message') {
+            addMessage(JSON.parse(data));
+          }
+        } catch (err) {
+          return;
+        }
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+});
+
+client.addEventListener('open', postMessage);
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import 'dotenv/config';
+import { nanoid } from 'nanoid';
 import { WebSocketServer } from 'ws';
 import http from 'http';
 import express from 'express';
+import { URL } from 'url';
+
+const DB = {
+  users: [
+    { id: 1, username: 'Anna', password: 'Anna' },
+    { id: 2, username: 'Victor', password: 'Victor' },
+    { id: 3, username: 'Sasha', password: 'Sasha' },
+  ],
+  tokens: {},
+};
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,13 +20,74 @@ app.use(express.static('public'));
 
 const server = http.createServer(app);
 
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({ clientTracking: false, noServer: true });
+const clients = new Map();
 
-wss.on('connection', (ws) => {
+app.post('/login', express.json(), (req, res) => {
+  const { username, password } = req.body;
+  const user = DB.users.find((u) => u.username === username);
+
+  if (!user) {
+    return res.status(401).send('Unknown username');
+  }
+  if (user.password !== password) {
+    return res.status(401).send('Wrong password');
+  }
+
+  const token = nanoid();
+  DB.tokens[token] = user.id;
+
+  res.json({ token });
+});
+
+server.on('upgrade', (req, socket, head) => {
+  const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
+  const token = searchParams && searchParams.get('token');
+  const userId = token && DB.tokens[token];
+
+  if (!userId) {
+    socket.write('HTTP/1.1 401 Unautorized\r\n\r\n');
+    socket.destroy();
+    return;
+  }
+
+  req.userId = userId;
+
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    wss.emit('connection', ws, req);
+  });
+});
+
+wss.on('connection', (ws, req) => {
+  const { userId } = req;
+
+  clients.set(userId, ws);
+
+  ws.on('close', () => {
+    clients.delete(userId);
+  });
+
   ws.on('message', (message) => {
-    wss.clients.forEach((ws) => {
-      ws.send(message);
-    });
+    let data;
+
+    try {
+      data = JSON.parse(message);
+    } catch (err) {
+      return;
+    }
+
+    if (data.type === 'chat_message') {
+      const user = DB.users.find((u) => u.id === userId);
+      const fullMessage = JSON.stringify({
+        type: 'chat_message',
+        message: data.message,
+        name: user.username,
+      });
+
+      for (ws of clients.values()) {
+        ws.send(fullMessage);
+      }
+    }
   });
 });
 
